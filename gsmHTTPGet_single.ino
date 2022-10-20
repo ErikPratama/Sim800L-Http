8c43bd32d9353eb7ecdce08feb84b935559b91c1
Erik Pratama <66062102+ErikPratama@users.noreply.github.com>
2022-10-20 07:59:02 +0700

    Add files via upload

---
+++ gsmHTTPGet_single.ino
@@ -0,0 +1,166 @@
+enum _parseState {
+  PS_DETECT_MSG_TYPE,
+
+  PS_IGNORING_COMMAND_ECHO,
+
+  PS_HTTPACTION_TYPE,
+  PS_HTTPACTION_RESULT,
+  PS_HTTPACTION_LENGTH,
+
+  PS_HTTPREAD_LENGTH,
+  PS_HTTPREAD_CONTENT
+};
+String Username = "";
+byte parseState = PS_DETECT_MSG_TYPE;
+char buffer[80];
+byte pos = 0;
+
+int contentLength = 0;
+
+void resetBuffer() {
+  memset(buffer, 0, sizeof(buffer));
+  pos = 0;
+}
+
+void sendGSM(const char* msg, int waitMs = 500) {
+  Serial2.println(msg);
+  delay(waitMs);
+  while(Serial2.available()) {
+    parseATText(Serial2.read());
+  }
+}
+
+void setup()
+{
+  Serial2.begin(9600);
+  Serial.begin(9600);
+  
+  sendGSM("AT+SAPBR=3,1,\"APN\",\"Internet\"");  
+  sendGSM("AT+SAPBR=1,1",3000);
+  sendGSM("AT+HTTPINIT");  
+  sendGSM("AT+HTTPPARA=\"CID\",1");
+   sendGSM("AT+HTTPPARA=\"URL\",\"http://fms3.cerindocorp.id/bacadata.php/?id=001\"");
+  //sendGSM("AT+HTTPPARA=\"URL\",\"http://www.iforce2d.net/test.php\"");
+  sendGSM("AT+HTTPACTION=0");
+ // Serial.println ("Username = " + Username);
+}
+
+void loop()
+{  
+}
+
+void parseATText(byte b) {
+  buffer[pos++] = b;
+  if ( pos >= sizeof(buffer) )
+    resetBuffer(); // just to be safe
+  switch (parseState) {
+  case PS_DETECT_MSG_TYPE: 
+    {
+      if ( b == '\n' )
+        resetBuffer();
+      else {        
+        if ( pos == 3 && strcmp(buffer, "AT+") == 0 ) {
+          parseState = PS_IGNORING_COMMAND_ECHO;
+        }
+        else if ( b == ':' ) {
+          //Serial.print("Checking message type: ");
+          //Serial.println(buffer);
+
+          if ( strcmp(buffer, "+HTTPACTION:") == 0 ) {
+            Serial.println("Received HTTPACTION");
+            parseState = PS_HTTPACTION_TYPE;
+          }
+          else if ( strcmp(buffer, "+HTTPREAD:") == 0 ) {
+            Serial.println("Received HTTPREAD");            
+            parseState = PS_HTTPREAD_LENGTH;
+          }
+          resetBuffer();
+        }
+      }
+    }
+    break;
+
+  case PS_IGNORING_COMMAND_ECHO:
+    {
+      if ( b == '\n' ) {
+        Serial.print("Ignoring echo: ");
+        Serial.println(buffer);
+        parseState = PS_DETECT_MSG_TYPE;
+        resetBuffer();
+      }
+    }
+    break;
+
+  case PS_HTTPACTION_TYPE:
+    {
+      if ( b == ',' ) {
+        Serial.print("HTTPACTION type is ");
+        Serial.println(buffer);
+        parseState = PS_HTTPACTION_RESULT;
+        resetBuffer();
+      }
+    }
+    break;
+
+  case PS_HTTPACTION_RESULT:
+    {
+      if ( b == ',' ) {
+        Serial.print("HTTPACTION result is ");
+        Serial.println(buffer);
+        parseState = PS_HTTPACTION_LENGTH;
+        resetBuffer();
+      }
+    }
+    break;
+
+  case PS_HTTPACTION_LENGTH:
+    {
+      if ( b == '\n' ) {
+        Serial.print("HTTPACTION length is ");
+        Serial.println(buffer);
+        
+        // now request content
+        Serial2.print("AT+HTTPREAD=0,");
+        Serial2.println(buffer);
+        
+        parseState = PS_DETECT_MSG_TYPE;
+        resetBuffer();
+      }
+    }
+    break;
+
+  case PS_HTTPREAD_LENGTH:
+    {
+      if ( b == '\n' ) {
+        contentLength = atoi(buffer);
+        Serial.print("HTTPREAD length is ");
+        Serial.println(contentLength);
+        
+        Serial.print("HTTPREAD content: ");
+        
+        parseState = PS_HTTPREAD_CONTENT;
+        resetBuffer(); 
+      }
+    }
+    break;
+
+  case PS_HTTPREAD_CONTENT:
+    {
+      // for this demo I'm just showing the content bytes in the serial monitor
+      Serial.write(b);
+      Username+=char(b);
+      
+      contentLength--;
+      
+      if ( contentLength <= 0 ) {
+
+        // all content bytes have now been read
+       Serial.println();
+       Serial.println( "Username =" + Username);
+        parseState = PS_DETECT_MSG_TYPE;
+        resetBuffer();
+      }
+    }
+    break;
+  }
+}
